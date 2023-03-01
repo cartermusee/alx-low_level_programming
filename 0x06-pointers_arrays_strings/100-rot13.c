6c8c9005740ee84d647ce0cbfb040a72ddeb44ff
cartermusee <cartermusee@gmail.com>
2023-03-01 16:34:23 +0000

    100-rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,26 +1,30 @@
 #include "main.h"
 
 /**
- * rot13 -  a function that encodes a string using rot13.
- * @s: An input string to encode using rot13
- * Return: An encode string
+ * rot13 - function that encodes a string using rot13.
+ * @s: pointer to char
+ * Return: char
  */
+
 char *rot13(char *s)
 {
 	int i = 0;
+	int j = 0;
+	char *l = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+	char *r13 = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
-	while (s[i] != '\0')
+	while (*(s + i) != '\0')
 	{
-		while ((s[i] >= 'a' && s[i] <= 'z') ||
-				(s[i] >= 'A' && s[i] <= 'Z'))
+		while (*(l + j) != '\0')
 		{
-			if ((s[i] >= 'a' && s[i] <= 'm') ||
-					(s[i] >= 'A' && s[i] <= 'M'))
-				s[i] += 13;
-			else
-				s[i] -= 13;
-			i++;
+			if (*(s + i) == *(l + j))
+			{
+				*(s + i)  = *(r13 + j);
+				break;
+			}
+			j++;
 		}
+		j = 0;
 		i++;
 	}
 	return (s);
