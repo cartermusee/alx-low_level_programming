5393cc915a093063bfd33008fecd5fbaac1c936f
cartermusee <cartermusee@gmail.com>
2023-03-30 12:08:52 -0700

    no.elemnts

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - function that returns the number of elements in a linked
+ * list_t list
+ * @h: header pointer
+ * Return: number of elements
+ */
+
+
+size_t list_len(const list_t *h)
+{
+	size_t  i = 0;
+
+	while(h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
