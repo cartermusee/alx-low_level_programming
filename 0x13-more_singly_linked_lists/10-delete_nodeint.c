841c300f5cc794f1510a58535e9de065f18edb34
cartermusee <cartermusee@gmail.com>
2023-04-04 11:11:45 -0700

    9 and 10

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index-function that deletes the node at index
+ * @head: pointer to a pointer to the head node of the list
+ *
+ * @index: index of the position of the node to be deleted
+ * Return: 1 if it succeeded, -1 if it failed
+ */
+
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *node, *tmp;
+
+	unsigned int i;
+
+	if  (index == 0 && *head != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
+		return (1);
+	}
+	node = *head;
+
+	for (i = 0; i < index - 1 && node != NULL; i++)
+	{
+		node = node->next;
+	}
+	if (node == 0 && node->next == NULL)
+	{
+		return (0);
+	}
+	 tmp = node->next;
+	 node->next = tmp->next;
+	 free(tmp);
+
+	return (1);
+}
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a given position
+ *
+ * @head: pointer to a pointer to the head node of the list
+ *
+ * @idx: index of the position where the new node should be inserted
+ * @n: value to be stored in the new node
+ *
+ * Return:the address of the new node, or NULL if it failed
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *new, *old;
+
+	unsigned int k;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
+	old = *head;
+	for (k = 0; k < idx && old != NULL; k++)
+	{
+		old = old->next;
+	}
+	if (old == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->next = old->next;
+	old->next = new;
+
+	return (new);
+}
