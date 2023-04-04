9babd833734a648aeb4826c19416ec81fd0fa290
cartermusee <cartermusee@gmail.com>
2023-04-04 11:33:12 -0700

    10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -30,7 +30,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	if (node == NULL || node->next == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 	 tmp = node->next;
 	 node->next = tmp->next;
