a0e98501e02d7eb3be5309d3c5e1d3496efd4409
cartermusee <cartermusee@gmail.com>
2023-04-07 10:48:15 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,14 +1,14 @@
 #include "main.h"
 #include <string.h>
 
+int _strlen(char *s);
 /**
  * create_file - function that creates a file
  * @filename:pointer to file
- * @text-content:is a NULL terminated string to write to the file
+ * @text_content: pointer to a NULL terminated string to write to the file
  * Return:1 on success, -1 on failure
  */
 
-int _strlen(char *s);
 
 int create_file(const char *filename, char *text_content)
 {
@@ -34,8 +34,8 @@ int create_file(const char *filename, char *text_content)
 	return (byte == length ? 1 : -1);
 }
 /**
- * _strlen - returns the length of a string
- * @s: the string whose length to check
+ * _strlen - function that gets the length of string
+ * @s: pointer to the text whose length to check
  *
  * Return: integer length of string
  */
