886788c7c98a3b95e18f36df8ab080d12f86723f
cartermusee <cartermusee@gmail.com>
2023-06-15 09:22:48 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "list.h"
+
+/**
+ * dlistint - function to get number of elemnts
+ * @h:node head
+ * Return:number of elemnts
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return(i);
+}
