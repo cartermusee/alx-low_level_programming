83dd62d0aa260aea4767668202aea4ba42268ca5
cartermusee <cartermusee@gmail.com>
2023-06-15 08:13:40 -0700

    number of elemnts

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - funtion to print he elements
+ * @h:pointer to list
+ * Return:number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int i = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (h);
+}
