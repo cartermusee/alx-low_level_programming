37e3dbcd892ef8b591a8e60d604bbffb6dd5c073
cartermusee <cartermusee@gmail.com>
2023-06-15 10:25:15 -0700

    3-add_dnodeint_end.c

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds node to list
+ * @head: address of ptr to head node
+ * @n: int  new node
+ *
+ * Return: address of new node or NULL
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+    dlistint_t *new;
+	dlistint_t *temp = *head;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+	if (!*head)
+	{
+		new->prev = NULL;
+		*head = new;
+	}
+	else
+	{
+		temp = *head;
+		while (temp->next)
+			temp = temp->next;
+
+		temp->next = new;
+		new->prev = temp;
+	}
+	return (new);
+}
