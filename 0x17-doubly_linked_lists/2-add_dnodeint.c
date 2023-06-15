c7554d2d7ae1013585c5b8db9ab5432883e1f11d
cartermusee <cartermusee@gmail.com>
2023-06-15 11:16:43 -0700

    2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -16,18 +16,18 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new = malloc(sizeof(dlistint_t));
 	if (!new || !head)
 	{
-		return (new ? free(new),NULL : NULL);
+		return (new ? free(new), NULL : NULL);
 	}
 	new->n = n;
 	new->prev = NULL;
 	if (*head == NULL)
 	{
-		*head = new;
+		temp = new;
 		new->next = NULL;
 	}
 	else
 	{
-		new->next = new;
+		new->next = temp;
 		temp->prev = new;
 		temp = new;
 	}
