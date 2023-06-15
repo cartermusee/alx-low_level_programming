bbef04e519e8b4c8fb64b5fa836c97eac0cd1e23
cartermusee <cartermusee@gmail.com>
2023-06-15 10:49:49 -0700

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - func returns node at an index
+ * @head: ptr to head node
+ * @index: index of node 
+ * Return: address of node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
+}
