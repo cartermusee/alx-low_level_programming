c93b35b2c105b399445ae5a0d23d70e8932bb218
cartermusee <cartermusee@gmail.com>
2023-06-20 00:47:51 -0700

    cpython

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://github.com/cartermusee/alx-low_level_programming/tree/master/0x18-dynamic_libraries/winlib.so
-export LD_PRELOAD=/tmp/winlib.so
-
+wget -P /tmp/ https://github.com/cartermusee/alx-low_level_programming/blob/master/0x18-dynamic_libraries/wishlib.so
+export LD_PRELOAD=./wishlib.so
