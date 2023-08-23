1809f28b2626779763aff8f62c806161ab63fe87
cartermusee <cartermusee@gmail.com>
2023-08-23 11:02:50 -0700

    py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -15,11 +15,7 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 perimeter += 4
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1
-                if i < row and grid[i + 1][j] == 1:
-                    perimeter -= 1
+                    perimeter -= 2
                 if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 1
-                if j < col - 1 and grid[i][j + 1] == 1:
-                    perimeter -= 1
+                    perimeter -= 2
     return perimeter
